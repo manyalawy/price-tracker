33781c77eef4101cafcd1b612b7b5376f27c96f6
Youssef <manyalawy@gmail.com>
2026-04-09 12:54:06 +0200

    chore: wire eslint-config-prettier, add .prettierignore

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: { node: true, es2022: true },
   parserOptions: { ecmaVersion: 2022 },
+  extends: ['prettier'],
   rules: {
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-var': 'error',
